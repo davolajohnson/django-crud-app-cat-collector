c1be1f9364bef84d03caf393d23b381e067b94b1
User Name <davolastagg@gmail.com>
2025-10-06 23:25:13 -0500

    Add home.html and home.css for home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views # views are similar to controllers
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about")
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  # route for cats index
+  path('cats/', views.cat_index, name='cat-index'),
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# views.py
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
 def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
-    return render(request, "about.html")
+    return render(request, "about.html") #DTL - Django Template Language # This checks the template
+
+# views.py
+
+def cat_index(request):
+    return render(request, 'cats/index.html', {'cats': cats}) # Render the cats/index.html template with the cats data
